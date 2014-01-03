e67103e9cbd64fd15fb1444e3f9bdf45b3887ab2
Wrongway <wrongway@Wrong-WaymatoMacBook-Pro.local>
2014-01-03 22:32:34 +0800

    modity the docs' theme

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
-print(os.path.abspath('..'))
+sys.path.append(os.path.abspath('../..'))
 
 # -- General configuration ------------------------------------------------
 
@@ -101,7 +101,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+#html_theme = 'nature'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
